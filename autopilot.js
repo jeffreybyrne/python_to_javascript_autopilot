46612a6e9b35c71d522f9a858a0e15146b1ff186
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-22 14:31:23 -0400

    converted a third function

--- autopilot.js
+++ autopilot.js
@@ -10,3 +10,9 @@ function add_car(cars, new_car) {
   cars.push(new_car)
   return `Adding new car to fleet. Fleet size is now ${cars.length}.`
 }
+
+function pick_up_passenger(car) {
+  car['passengers'] += 1;
+  car['gas'] -= 10;
+  return `Picked up passenger. Car now has ${car['passengers']} passengers.`
+}
